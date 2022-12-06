bf677bab67a8f9e9c031c31e00be264d6202b963
Senanes <sananaas@code.edu.az>
2022-12-06 17:13:19 +0400

    first commit

---
+++ js-lesson/assets/Js/app.js
@@ -0,0 +1,48 @@
+// let arr =  [2,3,6,9,10,12,15,17,19,20];
+// let odds = arr.filter(n => n%2==0)
+// let evens = arr.filter(n => n%2==1)
+
+// console.log(odds)
+
+// console.log(evens)
+
+// ---------------------------------------------------------------------------
+
+
+// let Find = function(a) {
+//   let arr = [2,3,6,9,10,12,15,17,19,20];
+//   let i = arr.indexOf(a)
+//   if(i === -1) {
+//     return "element yoxdur"
+//   } else{
+//     return i
+//   }
+// }
+
+// -------------------------------------------------------------------------
+
+
+
+// function palindromeFun (str )  
+// {  
+
+// const arrValue = string.split (''); 
+  
+
+// const reveArrVal = arrValue.reverse();   
+  
+
+// const revString = reveArrVal.join('');  
+  
+// if (string == revString)  
+// {  
+// alert('Palindromdur');  
+// }  
+// else {  
+// alert ('Palindrom deyil' );   
+// }  
+// }  
+
+// const string = prompt( 'Palindromu yoxla');  
+// const value = palindromeFun (string); 
+// console.log(value);  
